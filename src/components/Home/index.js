1f30fcb65d3a73880b10d2de91c3d6fd2672cd60
Kyle Pollich <kpollich1@gmail.com>
2017-06-19 23:51:07 -0400

    Run prettier

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Header from '../Header'
-import PostList from '../PostList'
+import Header from "../Header";
+import PostList from "../PostList";
 
 class Home extends Component {
   render() {
     return (
       <div>
         <Header />
-        
+
         <h2>Here are some posts</h2>
         <PostList />
       </div>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import registerServiceWorker from "./registerServiceWorker";
 
-import Home from './components/Home'
+import Home from "./components/Home";
 
-import './index.css';
+import "./index.css";
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById("root"));
 registerServiceWorker();
