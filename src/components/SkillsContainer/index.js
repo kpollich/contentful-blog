d6483ffe39dfc79167a5dfb4c0cec8968af25be4
Kyle Pollich <kpollich1@gmail.com>
2017-07-23 20:28:11 -0400

    Separate out SkillsContainer and IconLinks into components

---
+++ src/components/SkillsContainer/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+import SkillCard from '../SkillCard'
+
+import './SkillsContainer.css'
+
+class SkillsContainer extends Component {
+  render() {
+    return (
+      <div className="skills-container">
+        <h1 className="title has-text-centered">What I do</h1>
+
+        <div className="columns skillcards-container">
+          <div className="column">
+            <SkillCard
+              title="DevOps"
+              list={['AWS', 'Docker', 'Git', 'Solution Architecture']}
+            />
+          </div>
+          <div className="column">
+            <SkillCard
+              title="Backend"
+              list={[
+                'Node.js',
+                'Ruby on Rails',
+                'REST APIs',
+                'MySQL + Postgres',
+                'Redis'
+              ]}
+            />
+          </div>
+          <div className="column">
+            <SkillCard
+              title="Frontend"
+              list={['HTML', 'CSS', 'Javascript', 'React.js']}
+            />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SkillsContainer
