6d0cbbe468a1b2a14a081e69e9872158522eb32c
Kyle Pollich <kpollich1@gmail.com>
2017-08-15 22:39:21 -0400

    Revert "Break some homepage markup into components"
    This reverts commit c62be9caf9032aab7c07b269abbd63fdd3fa929d.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
-import Hero from '../../components/Hero'
-import HomeBlurb from '../../components/HomeBlurb'
+import IconLinks from '../../components/IconLinks'
 import Layout from '../../components/Layout'
 import SkillsContainer from '../../components/SkillsContainer'
-import Workplaces from '../../components/Workplaces'
 
 import './Home.css'
 
@@ -12,10 +11,88 @@ class Home extends Component {
   render() {
     return (
       <Layout>
-        <Hero />
-        <HomeBlurb />
+        <div
+          className="hero is-fullheight is-fullwidth has-text-centered"
+          style={{
+            backgroundImage: 'url(/img/firewatch-blue.png)',
+            backgroundPositionY: 'center'
+          }}
+        >
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title is-1" style={{ color: 'white' }}>
+                Kyle Pollich
+              </h1>
+              <h2 className="subtitle" style={{ color: 'white' }}>
+                Lancaster, PA Full Stack Developer
+              </h2>
+              <IconLinks />
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="container has-text-centered"
+          style={{ marginTop: '3em' }}
+        >
+          <div className="content is-medium">
+            <h1 className="title is-2">Who I Am</h1>
+            Hello, I'm Kyle! I am a Software Engineer and Full Stack Web
+            Developer currently located in Lancaster, Pennsylvania. I love
+            working with
+            with Ruby on Rails and Javascript and architecting elegant, scalable
+            web apps. If you're interested
+            in working with me or knowing a bit more about what I do, feel free
+            to contact me
+            via{' '}
+            <a href="mailto:kpollich1@gmail.com">email</a>.
+          </div>
+        </div>
+
         <SkillsContainer />
-        <Workplaces />
+
+        <div className="container">
+          <h1 className="title is-2 has-text-centered">Where I Work</h1>
+
+          <div className="columns where-i-work-container">
+            <div className="column">
+              <h2 className="title is-3 has-text-centered">
+                <a
+                  href="http://shiftlabny.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Shift Lab{' '}
+                </a>
+              </h2>
+
+              <p className="content is-medium">
+                Design minded development. Consulting, e-commerce platforms, and
+                a wide array of development services. At Shift, we provide a
+                compelling experience both for your users and your business.
+              </p>
+            </div>
+
+            <div className="column">
+              <h2 className="title is-3 has-text-centered">
+                <a
+                  href="https://www.pubforge.work"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Pubforge
+                </a>
+              </h2>
+
+              <p className="content is-medium">
+                Pubforge is a full service coworking space located in beautiful
+                downtown Lancaster. With tons of natural light and a fully
+                stocked kitchen, this space promotes collaboration and community
+                unlike any other.
+              </p>
+            </div>
+          </div>
+        </div>
       </Layout>
     )
   }
